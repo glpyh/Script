0e9162c7471c75961597e04142001ab8234e632c
glpyh <glpyhster@gmail.com>
2024-01-26 16:24:19 +0800

    Create streamMusic.js

---
+++ streamMusic.js
@@ -0,0 +1,34 @@
+/*
+#!name = 音流
+#!desc =  永久解锁VIP
+#!author = osinx
+#!homepage = https://github.com/osinx/Script
+#!icon = https://raw.githubusercontent.com/osinx/Script/main/icon/streamMusic.png
+
+[rewrite_local]
+^https?:\/\/pay\.aqzscn\.cn\/api\/v1\/payments\/ url script-response-body https://raw.githubusercontent.com/osinx/Script/main/vip/streamMusic.js
+
+[Mitm]
+hostname = play.aqzscn.cn
+*/
+
+let res = JSON.parse($response.body);
+
+const url = $request.url;
+const valiorder = "/validations/orders";
+const device = "/onlinedevices";
+
+if (url.indexOf(valiorder) != -1) {
+    res.originalTransactionId = "1704038400";
+    res.originalPurchaseDate = 1704038400;
+    res.email = "";
+    res.platform = "alipay";
+} else if (url.indexOf(device) != -1) {
+    res["Authorization-Date"] = 4102358400;
+    delete res.code
+    delete res.message
+}
+
+$done({
+    body: JSON.stringify(res)
+});
